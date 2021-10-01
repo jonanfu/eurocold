d413dcd8178719d46d4cd27186659dd98e7d9389
Jonathan <jonanfu@gmail.com>
2021-09-30 22:09:58 -0500

    subir archivos

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
+// own css files here
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
